f6731403321c2949a0aa767efbc44dd87e83eabc
Júlio Freitas <jcsr.frts@gmail.com>
2023-01-06 15:10:18 -0300

    add fibonacci class

---
+++ Fibonacci.java
@@ -0,0 +1,33 @@
+package org.example;
+
+public class Fibonacci {
+
+    private static long numberToStop;
+
+    private static long calculate(int number) {
+        if (number < 2) {
+            return number;
+        }
+
+        return calculate(number - 1) + calculate(number - 2);
+    }
+
+    private static boolean checkStop(long calculatedNumber) {
+        return calculatedNumber <= Fibonacci.numberToStop;
+    }
+
+    public static void print(int howManyNumbers, long numberToStop) {
+        Fibonacci.numberToStop = numberToStop;
+
+        for (int i = 0; i < howManyNumbers; i++) {
+            long currentValue = Fibonacci.calculate(i);
+
+            if (checkStop(currentValue)) {
+                System.out.print(currentValue + "  ");
+            } else {
+                break;
+            }
+        }
+    }
+
+}
